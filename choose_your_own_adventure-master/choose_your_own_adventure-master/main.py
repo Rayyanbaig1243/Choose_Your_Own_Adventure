eaaa845dfdd0ade3b91b7d5ffd142bf77de41b19
Rayyan Baig <137826567+Rayyanbaig1243@users.noreply.github.com>
2023-07-17 15:03:20 +0500

    Add files via upload

---
+++ choose_your_own_adventure-master/choose_your_own_adventure-master/main.py
@@ -0,0 +1,6 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure")
+
+answer = input("You come to an end road, you can only go left or right. Which way you want to go, left or right? ").lower()
+
+
